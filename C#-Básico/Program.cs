23141f04aa73f581ff454c3cb475491994e17724
EdersonLopes <91449431+EdersonLopes@users.noreply.github.com>
2023-06-14 01:42:10 -0300

    Create Program.cs
    Eu criei um programa em C# para demonstrar o conceito de herança. Comecei definindo uma classe base chamada Pessoa, que representa uma pessoa com atributos como nome, idade e vivo. Também incluí métodos como matar(), viver() e verificar(). Esses métodos me permitem alterar o estado da pessoa para morto ou vivo e verificar o status atual.
    
    Em seguida, eu criei uma classe chamada Paulista que herda da classe Pessoa. Essa classe adiciona o atributo localNasc e tem um construtor que recebe o nome da pessoa. No construtor, defini o local de nascimento como "São Paulo" e a idade como 27.
    
    Por fim, criei a classe Paulistano, que herda da classe Paulista. Essa classe adiciona o atributo privado id e tem um construtor padrão. No construtor, defini o nome como "Daniel" e o local de nascimento como "Osasco-SP". Também incluí métodos getId() e setId(), que permitem obter e definir o valor do atributo id.
    
    No método Main, criei instâncias das classes Pessoa, Paulista e Paulistano e usei os métodos e atributos definidos para exibir informações na tela.
    
    Espero que essa descrição em primeira pessoa tenha sido útil para entender o programa que eu escrevi.

---
+++ C#-Básico/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+     internal class Program
+    {
+        public class Pessoa
+        {
+            public string nome;
+            public int idade;
+            public bool vivo;
+
+        
+            
+            
+        public void matar()
+            {
+                vivo = false;
+            }
+        public void viver() {
+                
+                vivo = true;
+            
+            }
+        public string verificar() {
+            if (vivo == true ) {
+                    return "Vivo";
+            }else 
+                {   return "Morto";
+                }
+            
+            }
+        }
+        public class Paulista : Pessoa
+        {
+            public string localNasc;
+
+            public Paulista(string nome)
+            {
+                this.nome = nome;
+                localNasc = "São Paulo";
+                idade = 27;
+            }
+
+        }
+            public class Paulistano : Paulista
+
+        {
+            private int id;
+
+            public Paulistano() : base("Daniel") {
+                localNasc = "Osasco-SP";
+                vivo = true;
+            
+            
+            }
+            public int getId()
+            {
+                return id;
+            }
+           
+            public int setID(int id)
+            {
+                this.id = id;
+                return id;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Ederson";
+            p1.idade = 26;
+            p1.viver();
+            Console.WriteLine("Nome:........: {0}", p1.nome);
+            Console.WriteLine("Idade:.......: {0}", p1.idade);
+            Console.WriteLine("Status:......: {0}", p1.verificar());
+            Console.ReadLine();
+            
+            Paulista p2 = new Paulista("Daniel");
+            p1.idade = 32;
+            p2.localNasc = "São Paulo";
+            p2.viver();
+            Console.WriteLine("Nome:........: {0}", p2.nome);
+            Console.WriteLine("Idade:.......: {0}", p2.idade);
+            Console.WriteLine("Status:......: {0}", p2.verificar());
+            Console.WriteLine("Local de Nas:..: {0}", p2.localNasc);
+            Console.ReadLine() ;
+
+            Paulistano p3 = new Paulistano();
+            p3.setID(10987);
+
+            Console.WriteLine("Nome:........: {0}", p3.nome);
+            Console.WriteLine("Idade:.......: {0}", p3.idade);
+            Console.WriteLine("Status:......: {0}", p3.verificar());
+            Console.WriteLine("Local de Nas:.:{0}", p3.localNasc);
+            Console.WriteLine("ID............:{0}", p3.getId());
+            Console.ReadLine();
+        }
+    }
+}
